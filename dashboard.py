78963013c2a72ac78fc433be568f641e871923eb
Julien Ouertani <ouertani.julien@gmail.com>
2025-10-15 14:30:17 +0200

    Initial commit

---
+++ dashboard.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+import plotly.graph_objects as go   
+
+
+st.set_page_config(page_title="Dashboard", 
+                   page_icon=":bar_chart:",
+                   layout="wide")
+st.sidebar.header("Header")
+st.sidebar.success("Success message!")
